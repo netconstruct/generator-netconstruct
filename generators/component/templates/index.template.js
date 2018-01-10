7b6ca5ce5643a55751a8f51cb45563f0f5181897
Richard Shackleton <richardshackleton@netconstruct.co.uk>
2018-01-10 10:34:13 +0000

    Remove unused generators

--- generators/component/templates/index.template.js
+++
@@ -1,8 +0,0 @@
-import controller from './<%= componentDashedName %>.controller';
-import template from './<%= componentDashedName %>.view.html';
-
-export default {
-  controller,
-  controllerAs: 'vm',
-  template,
-};
