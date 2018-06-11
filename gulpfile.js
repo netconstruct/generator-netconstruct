ab2308aed16010e349868cbc7a92e3724c05789c
Richard Shackleton <richardshackleton@netconstruct.co.uk>
2018-06-11 08:46:57 +0100

    Remove growl via gulp-mocha
    Github flagged growl as a vulnerability

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var path = require('path');
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
-var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
 var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
@@ -29,23 +28,6 @@ gulp.task('pre-test', function () {
       includeUntested: true
     }))
     .pipe(istanbul.hookRequire());
-});
-
-gulp.task('test', ['pre-test'], function (cb) {
-  var mochaErr;
-
-  gulp.src('test/**/*.js')
-    .pipe(plumber())
-    .pipe(mocha({
-      reporter: 'spec'
-    }))
-    .on('error', function (err) {
-      mochaErr = err;
-    })
-    .pipe(istanbul.writeReports())
-    .on('end', function () {
-      cb(mochaErr);
-    });
 });
 
 gulp.task('coveralls', ['test'], function () {
