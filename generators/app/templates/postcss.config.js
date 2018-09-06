1a5fce70aac529c04942e464ecf859b78207c980
Romain Fournioux <romainfournioux@netconstruct.co.uk>
2018-09-06 12:48:25 +0100

    Remove Gridle, add css grid example, add new media queries mixin

--- generators/app/templates/postcss.config.js
+++ generators/app/templates/postcss.config.js
@@ -1,5 +1,7 @@
 const autoprefixer = require('autoprefixer');
 const pseudoelements = require('postcss-pseudoelements');
+const normalize = require('postcss-normalize');
+const ie11 = require('postcss-ie11-supports');
 
 module.exports = {
   plugins: [
@@ -7,5 +9,7 @@ module.exports = {
       browsers: ['ie 11', 'last 2 versions'],
     }),
     pseudoelements(),
+    normalize({ forceImport: true }),
+    ie11(),
   ],
 };
